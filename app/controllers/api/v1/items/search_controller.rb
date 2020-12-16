89288d653765674368597dcbe2268d80e14e1d87
Brett Turing <bjsherman80@gmail.com>
2020-12-15 21:07:25 -0700

    add item find/show action using find_by

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -4,4 +4,9 @@ class Api::V1::Items::SearchController < ApplicationController
     items = Item.where('name ilike ?', "%#{params[:name]}%")
     render json: ItemSerializer.new(items)
   end
+
+  def show
+    items = Item.find_by('name ilike ?', "%#{params[:name]}%")
+    render json: ItemSerializer.new(items)
+  end
 end
