a93f59018cd8dcca51514d3683439af2200a21c7
Brett Turing <bjsherman80@gmail.com>
2020-12-15 18:42:23 -0700

    create merchant factory

--- spec/factories/merchants.rb
+++ spec/factories/merchants.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :merchant do
     name { Faker::Company.name }
+
+    trait :with_items do
+      transient do
+        items { 2 }
+      end
+      after :create do |merchant, evaluator|
+        create_list(:item, evaluator.items, merchant: merchant)
+      end
+    end
   end
 end
